6fd4b468318c5fb34c4f4aa0709f1f14cf3401f9
Kam300 <kamilgaripov205@gmail.com>
2024-10-11 21:15:47 +0500

    Signed-off-by: Kam300 <kamilgaripov205@gmail.com>

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RedBackground_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = Brushes.Red;
+            StatusText.Text = "Фон установлен на красный";
+        }
+
+        private void GreenBackground_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = Brushes.Green;
+            StatusText.Text = "Фон установлен на зеленый";
+        }
+
+        private void BlueBackground_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = Brushes.Blue;
+            StatusText.Text = "Фон установлен на синий";
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Автор: Гарипов Камиль", "Информация о разработчике");
+            StatusText.Text = "Открыто окно с информацией о разработчике";
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
